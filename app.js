7f0a7c6723d0413dba71ebf78d90cb0268277570
Victoria Soto <vicky_yeah_12@hotmail.com>
2023-06-28 18:22:46 -0600

    Ajuste puerto

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path")
 const app = express();
 const publicPath = path.resolve(__dirname,"./public");
+const port = process.env.PORT || 3001;
 
 app.use(express.static(publicPath));
 
@@ -23,4 +24,4 @@ app.get('/login',(req, res)=>{
     res.sendFile(path.join(__dirname,"views/login.html"))
 })
 
-app.listen(3000,()=> console.log("Escuchando en http://localhost:3000"))
+app.listen(port,()=> console.log(`Servidor corriendo en el puerto ${port}`))
